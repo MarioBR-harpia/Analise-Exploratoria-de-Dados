6bdafcc8338974ef939ac6490c19df97d76514ce
Mario Carvalho <64367592+MarioBR-harpia@users.noreply.github.com>
2025-02-21 15:07:21 -0400

    Add files via upload

---
+++ aed.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os 
+
+os.chdir('c:\Users\mvsvi\Desktop\Analise de Dados\Planilhas')
+
+dados = pd.read_csv("analise_exploratoria_dados.csv")
